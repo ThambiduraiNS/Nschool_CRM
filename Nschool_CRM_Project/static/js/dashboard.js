49db854499a583b479a0618751654161a98727ca
ThambiduraiNS <thambidurai@n-school.com>
2024-07-10 18:07:01 +0530

    update
    side navbar update

--- Nschool_CRM_Project/static/js/dashboard.js
+++ Nschool_CRM_Project/static/js/dashboard.js
@@ -1,11 +1,26 @@
- // JavaScript to handle arrow rotation
- console.log("ding")
- document.querySelectorAll('.dropdown-toggle').forEach(function (dropdown) {
-    console.log("Welcome !")
-    dropdown.addEventListener('click', function () {
-        let arrow = this.querySelector('.right_arrow');
-        if (arrow) {
-            arrow.style.transform = this.getAttribute('aria-expanded') === 'true' ? 'rotate(0deg)' : 'rotate(90deg)';
-        }
-    });
-});
+// function toggleDropdown(element) {
+//     // Close all dropdowns
+//     console.log("hello")
+//     const allDropdowns = document.querySelectorAll('.dropdown-menu');
+//     console.log(allDropdowns)
+//     allDropdowns.forEach(dropdown => {
+//         if (dropdown !== element.nextElementSibling) {
+//             dropdown.classList.remove('show');
+//         }
+//     });
+
+//     // Close all arrows
+//     const allArrows = document.querySelectorAll('.right_arrow');
+//     allArrows.forEach(arrow => {
+//         if (arrow !== element.querySelector('.right_arrow')) {
+//             arrow.classList.remove('arrow_user');
+//         }
+//     });
+
+//     // Toggle the clicked dropdown and arrow
+//     const dropdownMenu = element.nextElementSibling;
+//     dropdownMenu.classList.toggle('show');
+//     dropdownMenu.allDropdowns.style.transition = "all 2s";
+//     const rightArrow = element.querySelector('.right_arrow');
+//     rightArrow.classList.toggle('arrow_user');
+// }
