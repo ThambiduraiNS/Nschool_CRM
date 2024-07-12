8d18f3e13d862c28a9d5d74b333595092a289fd1
ThambiduraiNS <thambidurai@n-school.com>
2024-07-12 17:13:28 +0530

    update user module
    user module update

--- Nschool_CRM_Project/Nschool_CRM/urls.py
+++ Nschool_CRM_Project/Nschool_CRM/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('user_module/', views.user_module_view, name='user-module'),
 ]
--- Nschool_CRM_Project/Nschool_CRM/views.py
+++ Nschool_CRM_Project/Nschool_CRM/views.py
@@ -32,30 +32,6 @@ def dashboard_view(request):
     
     return render(request, 'dashboard.html',  { "datapoints" : json.dumps(datapoints), "datapoints2": json.dumps(datapoints2) })
 
-     
-# def multi_column_view(request):
-#   datapoints = [
-#     { "x": 10, "y": 171 },
-#     { "x": 20, "y": 155},
-#     { "x": 30, "y": 150 },
-#     { "x": 40, "y": 165 },
-#     { "x": 50, "y": 195 },
-#     { "x": 60, "y": 168 },
-#     { "x": 70, "y": 128 },
-#     { "x": 80, "y": 134 },
-#     { "x": 90, "y": 114}
-#   ]
- 
-#   datapoints2 = [
-#     { "x": 10, "y": 71 },
-#     { "x": 20, "y": 55},
-#     { "x": 30, "y": 50 },
-#     { "x": 40, "y": 65 },
-#     { "x": 50, "y": 95 },
-#     { "x": 60, "y": 68 },
-#     { "x": 70, "y": 28 },
-#     { "x": 80, "y": 34 },
-#     { "x": 90, "y": 14 }
-#   ]
- 
-#   return render(request, 'column_chart/multi_column.html', { "datapoints" : json.dumps(datapoints), "datapoints2": json.dumps(datapoints2) })
+
+def user_module_view(request):
+    return render(request, 'user_module.html')
